52d87bcdd79cd1bc36c737101991b1ce3d19baf0
MIKATYPE <PAF03112@nifty.com>
2024-01-19 14:17:55 +0900

    Version 2.02.02

---
+++ MIKASHORT.js
@@ -0,0 +1,64 @@
+﻿MIKA_c_line1=[
+"オズの魔法使い",
+" Dorothy lived in the midst of the great Kansas prairies,",
+"with Uncle Henry, who was a farmer, and Aunt Em, who was",
+"the farmer's wife. Their house was small, for the lumber",
+"to build it had to be carried by wagon many miles. There",
+"were four walls, a floor and a roof, which made one room;",
+"and this room contained rusty looking cooking stove, a",
+"cupboard for the dishes, a table, three or four chairs,",
+"and the beds."
+];
+MIKA_c_line2=[
+"不思議の国のアリス",
+" Alice was beginning to get very tired of sitting by her",
+"sister on the bank, and of having nothing to do: once or",
+"twice she had peeped into the book her sister was reading,",
+"but it had no pictures or conversations in it, \"and what",
+"is the use of a book,\" thought Alice, \"without pictures or",
+"conversation?\""
+];
+MIKA_c_line3=[
+"鏡の国のアリス",
+" One thing was certain, that the white kitten had had",
+"nothing to do with it:-it was the black kitten's fault",
+"entirely. For the white kitten had been having its face",
+"washed by the old cat for the last quarter of an hour (and",
+"bearing it pretty well, considering); so you see that it",
+"couldn't have had any hand in the mischief."
+];
+MIKA_c_line4=[
+"若草物語",
+" \"Christmas won't be Christmas without any presents,\"",
+"grumbled Jo, lying on the rug.",
+" \"It's so dreadful to be poor!\" sighed Meg, looking down",
+"at her old dress.",
+" \"I don't think it's fair for some girls to have plenty",
+"of pretty things, and other girls nothing at all,\" added",
+"little Amy, with an injured sniff.",
+" \"We've got father and mother and each other,\" said Beth,",
+"contentedly, from her corner."
+];
+MIKA_c_line5=[
+"足長おじさん",
+" The first Wednesday in every month was a Perfectly Awful",
+"Day-a day to be awaited with dread, endured with courage,",
+"and forgotten with haste. Every floor must be spotless,",
+"every chair dustless, and every bed without a wrinkle.",
+"Ninety-seven squirming little orphans must be scrubbed and",
+"combed and buttoned into freshly starched ginghams; and",
+"all ninety-seven reminded of their manners, and told to",
+"say \"Yes, sir,\" \"No, sir,\" whenever a trustee spoke."
+];
+MIKA_c_line6=[
+"幸福な王子",
+" High above the city, on a tall column, stood the statue",
+"of the Happy Prince. He was gilded all over with thin",
+"leaves of fine gold, for eyes he had two bright sapphires,",
+"and a large red ruby glowed on his sword-hilt.",
+" He was very much admired indeed. \"He is as beautiful as",
+"a weathercock,\" remarked one of the Town Councillors who",
+"wished to gain a reputation for having artistic tastes;",
+"\"only not quite so useful,\" he added, fearing lest people",
+"should think him unpractical, which he really was not."
+];
